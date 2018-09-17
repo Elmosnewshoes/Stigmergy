d1fb9b01b757ff8bebef7bf996ec715a8b59c5db
Bram <durieux.bram@gmail.com>
2018-09-17 11:43:57 +0200

    Made quick and dirty multi-ant-simulation

--- simulation.py
+++ simulation.py
@@ -1,3 +1,8 @@
+""" ==========
+    Written by: Bram Durieux,
+    Delft University of Technology, Delft, The Netherlands
+    ========== """
+
 from ant import Ant
 from domain import AntDomain
 from plot import MapPlot
@@ -5,7 +10,7 @@ import numpy as np
 # from overlord import Queen
 
 class SimpleSim():
-    def __init__(self, size = [1000,1000], pitch = 1):
+    def __init__(self, size = [1000,1000], pitch = 1, n_ants = 5):
         """ ==================
             Do a simulation of an Ant
             ================== """
@@ -16,14 +21,31 @@ class SimpleSim():
                              food = {'location': [850,500],'radius':50},
                              nest = {'location': [150,500],'radius':50})
         self.Dom.set_gaussian(sigma = 10)
-        self.Ant = Ant(limits = size, start_pos = [700,500], speed=10, v_max =20)
+        self.Ant = Ant(limits = size, start_pos = [700,500], speed=5,)
+        self.Ants = [ Ant(limits = size,
+                          start_pos = 1000*np.random.rand(1,2)[0],
+                          angle = 360*np.random.rand(), speed=2,
+                          v_max =5, ant_id = i) for i in range(n_ants) ]
         self.Plot = MapPlot(self.Dom.Map.X,self.Dom.Map.Y)
 
 
     def draw_ant_scatter(self):
-        self.Plot.draw_scatter(self.Ant.pos.x, self.Ant.pos.y)
-        self.Plot.draw_scatter(self.Ant.sensors['left'].x, self.Ant.sensors['left'].y, marker = 'o', name = 'sensor_left')
-        self.Plot.draw_scatter(self.Ant.sensors['right'].x, self.Ant.sensors['right'].y, marker = 'o', name = 'sensor_right')
+        ant_x =[]
+        ant_y =[]
+        sensor_left_x = []
+        sensor_left_y = []
+        sensor_right_x = []
+        sensor_right_y = []
+        for Ant in self.Ants:
+            ant_x.append(Ant.pos.x)
+            ant_y.append(Ant.pos.y)
+            sensor_left_x.append(Ant.sensors['left'].x)
+            sensor_left_y.append(Ant.sensors['left'].y)
+            sensor_right_x.append(Ant.sensors['right'].x)
+            sensor_right_y.append(Ant.sensors['right'].y)
+        self.Plot.draw_scatter(ant_x, ant_y)
+        self.Plot.draw_scatter(sensor_left_x, sensor_left_y, marker = 'o', name = 'sensor_left')
+        self.Plot.draw_scatter(sensor_right_x, sensor_right_y, marker = 'o', name = 'sensor_right')
 
     def random_roll_sim(self,n_steps = 500, contour_interval = 1):
         """ ==================
@@ -50,10 +72,9 @@ class SimpleSim():
             First, prime the map, then let the ant free
             ================== """
         # == Prime the map ==
-        self.Ant.v =self.Ant.v /5
         for i in range(50):
             loc = self.size*np.random.rand(1,2)[0]
-            self.Dom.add_pheromone(Q =0.25,sigma = 50,loc=loc,peak_1=True)
+            self.Dom.add_pheromone(Q =0.125,sigma = 50,loc=loc,peak_1=True)
         self.Dom.update_pheromone()
         self.Plot.draw_contour(self.Dom.Map.map)
 
@@ -69,34 +90,36 @@ class SimpleSim():
         # == loop over the simulation ==
         for i in range(n_steps):
             # get the pheromone level at ant sensor location
-            pheromone = self.Dom.get_pheromone_level(self.Ant.sens_loc,
-                                                     islist=True)
-            # perform a step
-            self.Ant.gradient_step([self.Ant.observed_pheromone(pheromone[0],activation = 'linear'),
-                                    self.Ant.observed_pheromone(pheromone[1],activation = 'linear')],
-                                   gain = 0.0125,
-                                   SNR = 0.0125)
-           # add the pheromone at ant location
-           # dont add pheromon when at the boundary!
-            if not self.Ant.out_of_bounds:
-                self.Dom.local_add_pheromone(self.Ant.pos.vec, peak_1 = True, Q = 0.1) # add pheromone to the map
-                self.Dom.update_pheromone() # update the global map
+            for Ant in self.Ants:
+                pheromone = self.Dom.get_pheromone_level(Ant.sens_loc,
+                                                         islist=True)
+                # perform a step
+                Ant.gradient_step([Ant.observed_pheromone(pheromone[0],activation = 'linear'),
+                                    Ant.observed_pheromone(pheromone[1],activation = 'linear')],
+                                       gain = 0.005,
+                                       SNR = 0.05)
+               # add the pheromone at ant location
+               # dont add pheromon when at the boundary!
+                if not Ant.out_of_bounds:
+                    self.Dom.local_add_pheromone(Ant.pos.vec, peak_1 = True, Q = 0.05) # add pheromone to the map
+                    self.Dom.update_pheromone() # update the global map
 
-            # == Some simulation specific intelligence ==
-            if self.Ant.foodbound and self.Dom.inrange(self.Ant.pos.vec,'food'):
-                self.Ant.foodbound = False
-                self.Ant.reverse()
-                print("Found food, looking for nest")
-            elif not(self.Ant.foodbound) and self.Dom.inrange(self.Ant.pos.vec, 'nest'):
-                self.Ant.foodbound = True
-                self.Ant.reverse()
-                print("Found nest, looking for food")
+                # == Some simulation specific intelligence ==
+                if Ant.foodbound and self.Dom.inrange(Ant.pos.vec,'food'):
+                    Ant.foodbound = False
+                    Ant.reverse()
+                    print("Found food, looking for nest")
+                elif not(Ant.foodbound) and self.Dom.inrange(Ant.pos.vec, 'nest'):
+                    Ant.foodbound = True
+                    Ant.reverse()
+                    print("Found nest, looking for food")
 
             # == draw stuff ==
             self.draw_ant_scatter()
             if i%contour_interval ==0:
                 print("At step {:d}".format(i))
                 self.Plot.draw_contour(self.Dom.Map.map)
+                self.Dom.evaporate(0.97)
 
 
 class MultiSim():
@@ -117,7 +140,7 @@ class MultiSim():
 
 
 def runSimpleSim():
-    S = SimpleSim()
+    S = SimpleSim(n_ants = 5)
     # S.random_roll_sim()
     S.gradient_sim(contour_interval = 10, n_steps = 1500)
 
