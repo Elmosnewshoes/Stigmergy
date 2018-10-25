fce97828e1116da51ce3b0087c19a939ae19850e
Bram Durieux <durieux.bram@gmail.com>
2018-10-25 17:17:09 +0200

    splitted the guassian Map method to its own child-class

--- entropy/core/plugins/helper_classes.py
+++ entropy/core/plugins/helper_classes.py
@@ -1,5 +1,8 @@
 import numpy as np
-from helper_functions import bivariate_normal
+if __name__ == '__main__':
+    from helper_functions import bivariate_normal
+else:
+    from plugins.helper_functions import bivariate_normal
 
 class point:
     """ holds xy coordinates in mm (floats)"""
@@ -21,16 +24,16 @@ class loc(point):
 
 
 class MeshMap:
-    def __init__(self,dim,resolution, R=0):
+    def __init__(self,dim,resolution):
         """ Dim: x and y limits in mm
             resolution: points per mm
-            R: radius in X,Y direction [mm], if nonzero, overwrites dim """
+         """
 
         " Properties "
-        if R ==0:
+        if type(dim) is list:
             self.dim = point(*dim) # in mm
         else:
-            self.dim = point(2*R,2*R) # in mm
+            self.dim = dim #assume already point object
         self.pitch = resolution # points per mm
         self.lim = loc(*np.round(np.dot(self.dim.vec,1/resolution),0)+[1,1])
 
@@ -40,31 +43,13 @@ class MeshMap:
         )
         self.map = np.zeros(self.mesh_x.shape)
 
-    def set_map(self,map_type,**type_kwargs):
-        if map_type =='identity':
-            self.map = np.identity(min(self.dim.x,self.dim.y))
-        elif map_type == 'zero':
-            self.map = np.zeros(self.mesh_x.shape)
-        elif map_type == 'random':
-            if kwargs['gain']:
-                gain = kwargs['gain']
-            else:
-                gain = 1
-            self.map = gain*np.random.rand(self.dim.x,self.dim.y)
-        elif map_type == 'gaussian':
-            self.map = self.pitch**2*bivariate_normal(self.mesh_x,self.mesh_y, type_kwargs['covariance'],
-                                              type_kwargs['covariance'],
-                                              (self.dim.x)/2,
-                                              (self.dim.y)/2)
-
-
     def coord2grid(self, pnt):
         """ return xy in grid coordinates (loc tuple) [absolute -- integer]
             from point coordinates (point tuple) [mm -- float] """
 
         """ Warning: floating point representation can cause
                 unexpected rounding results !!! """
-        return loc(*pnt.vec.dot(1/self.pitch).round(0).astype(int))
+        return loc(*np.rint(pnt.vec.dot(1/self.pitch)))
 
     def grid2coord(self, l):
         """ return xy in local coordinates (point tuple) [mm -- float]
@@ -81,7 +66,8 @@ class MeshMap:
     def span(self, l, R):
         """ return the upper limits of a meshmap that fits in self.map
             loc as loc tuple xy -> grid coords[int, int],
-            R as offset in grid coords [int] """
+            R as offset in mm """
+        R = np.asscalar(np.asarray(np.rint(R/self.pitch),dtype=int))
         x_min,y_min = [0,0]
         x_max,y_max = [2*R+1, 2*R+1]
         if l.x +R+1 > self.lim.x: x_max = x_max-(l.x+R - self.lim.x)-1
@@ -90,8 +76,24 @@ class MeshMap:
         if l.y -R < 0: y_min = R - l.y
         return {'x': [x_min,R,x_max],
                  'y': [y_min,R,y_max]}
+    def __repr__(self):
+        return "A map with size {}x{}; with resolution {}".format(*self.map.shape,self.pitch)
 
+class GaussMap(MeshMap):
+    def __init__(self,resolution,R,covariance):
+        """ Initialize the MeshMap, dim based on R, overwrite the map property """
+        " R in mm "
+        self.radius = R
+        dim = point(2*R,2*R)
+        super().__init__(dim,resolution)
+        self.map = self.gaussian_map(covariance) # override the zero map
+        self.volume = self.map.sum()*self.pitch**2
+        self.peak = self.map.max()
 
+    def gaussian_map(self, cov):
+        """ Return gaussian with volume ==1 """
+        return bivariate_normal(self.mesh_x,self.mesh_y, cov,
+                                 cov,(self.dim.x)/2,(self.dim.y)/2)
 
 def run():
     """ test stuff """
@@ -100,32 +102,31 @@ def run():
     print(p.vec)
     print(l.vec)
 def test_mesmap():
-    M = MeshMap([20,20], 0.5)
+    M = MeshMap([20,20], 2)
     print(M.lim.vec)
     R = 2
     target = loc(10,10)
     s = M.span(target ,R)
     print(s)
-    X = MeshMap([1,1], 2, 4)
     print(s['x'])
     print(s['y'])
     print(M.map)
-    X.set_map(map_type = 'gaussian',covariance = 4)
-    print(X.map)
+    X = GaussMap(resolution = 2,R = R, covariance = 2)
     tmp_map = X.map[s['y'][0]:s['y'][2],s['x'][0]:s['x'][2]]
-    print(tmp_map.shape)
+    print(tmp_map)
     print(M.map[target.y-s['y'][1]+s['y'][0]:target.y+s['y'][2]-s['y'][1],
                 target.x-s['x'][1]+s['x'][0]:target.x+s['x'][2]-s['x'][1]].shape)
     print(s)
     print(target.x-s['x'][1]+s['x'][0])
     print(target.x+s['x'][2]-s['x'][1])
-    print(target.y-s['y'][1]+s['y'][0])
-    print(target.y+s['y'][2]-s['y'][1])
+    # print(target.y-s['y'][1]+s['y'][0])
+    # print(target.y+s['y'][2]-s['y'][1])
 
     M.map[target.y-s['y'][1]+s['y'][0]:target.y+s['y'][2]-s['y'][1],
                 target.x-s['x'][1]+s['x'][0]:target.x+s['x'][2]-s['x'][1]] += tmp_map
     print(M.map)
     print(M.grid2coord(loc(1,1)))
-    print(M.coord2grid(point(1,1)))
+    print(M.coord2grid(point(3,3)))
+    print(M)
 if __name__ == '__main__':
     test_mesmap()
