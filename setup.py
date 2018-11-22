98010e7c9a42091965fa0ef381436aa08cba94b2
Bram Durieux <durieux.bram@gmail.com>
2018-11-22 14:55:03 +0100

    cleanup continues
    Former-commit-id: bbf38d20d90f86b09c30d7534305cfa6dfb6e6ec

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from Cython.Build import cythonize
 
 setup(name = 'location modules',
       ext_modules = cythonize(['cythonic/plugins/*.pyx',
-                              'cythonic/wrapper.pyx',
+                              'cythonic/*.pyx',
                               'cythonic/core/*.pyx',
 	]))
