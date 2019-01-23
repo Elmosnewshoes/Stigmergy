6ef2404d84239392a0882a0cf30cd8cc74828514
Bram Durieux <durieux.bram@gmail.com>
2019-01-23 11:22:38 +0100

    added default settings dictionaries
    Former-commit-id: e8e5f891ff748fd012ee401e6f9d0edcbe40d957

---
+++ cythonic/plugins/dummy_dicts.py
@@ -0,0 +1,24 @@
+ant_dict = {
+    'l': 5.,
+    'sens_offset': 45.,
+    'gain': 1.,
+    'drop_quantity': 1.,
+    'return_factor': 1,
+    'drop_beta':.2
+}
+
+queen_dict = {
+    'n': 10,
+    'dt': .5,
+    'ant_dict': ant_dict
+}
+
+domain_dict = {
+    'size': [1000,1000],
+    'pitch': .5,
+    'nest_loc': [250,500],
+    'nest_rad': 50,
+    'food_loc': [750,500],
+    'food_rad': 50,
+    'target_pheromone': 1.
+}
