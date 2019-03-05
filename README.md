8802ad2b20b955eb86e7108db51a1410c4ff156d
Bram Durieux <durieux.bram@gmail.com>
2019-03-05 13:04:22 +0100

    install scripts for the database

--- README.md
+++ README.md
@@ -10,7 +10,16 @@ user@computer:/../ANT$ python3 gui.py
 ```
 
 # Install instructions (Unix only)
-A Sqlite3 database is required ... more to follow
+Based on python3
+ - (optional) make sure pip3 is installed: ```sudo apt-get install python3-pip```
+ - (optional) use virtual environment, make sure virtualenv is available: ``` sudo apt install virtualenv ```
+ - (optional) DB browser for sqlite ``` sudo apt install sqlitebrowser ```
+ - (optional) Make a virtual environment ``` virtualenv theANT3000 -p python3 ``` and ``` source theANT3000/bin/activate ```
+ - Install dependencies: ``` pip3 install ...```
+ - Clone the project:
+```shell
+  user@computer:~$ git clone https://github.com/Elmosnewshoes/Stigmergy/ theANT3000
+  ```
 
 ## Package dependencies
  - numpy
---
+++ deploy.py
@@ -0,0 +1,38 @@
+# =======================================
+# Created by: Bram Durieux
+#   as part of the master thesis at the Delft University of Technology
+#
+# Description: Deploy the database used for the simulator
+# =======================================
+
+import sqlite3, sys
+from sqlite3 import Error
+
+
+def create_connection(db_file, qrys):
+    """ create a database connection to a SQLite database """
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+        cursor = conn.cursor()
+        for q in qrys:
+            cursor.execute(q)
+            print(q)
+    except Error as e:
+        print(e)
+    finally:
+        conn.close()
+
+if __name__ == '__main__':
+    qry = ["CREATE TABLE \"sim\" ( `ID` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `status` TEXT NOT NULL DEFAULT 'INITIALIZED', `recording` TEXT, `timestamp` DATETIME DEFAULT CURRENT_TIMESTAMP, `initializer` TEXT, `steps_recorded` INTEGER )"]
+    qry.append("CREATE TABLE `sim_settings` ( `sim_id` INTEGER NOT NULL UNIQUE, `n_agents` integer, `dt` numeric, `steps` integer, `deploy_style` text, `deploy_timing` text, `deploy_timing_args` text, `evap_rate` numeric, FOREIGN KEY(`sim_id`) REFERENCES `sim`(`ID`) ON DELETE CASCADE, PRIMARY KEY(`sim_id`) )")
+    qry.append("CREATE TABLE \"step\" ( `SIM_ID` INTEGER NOT NULL, `STEP_NR` INTEGER NOT NULL, `ANT_ID` INTEGER NOT NULL, `X` NUMERIC NOT NULL, `Y` NUMERIC NOT NULL, `THETA` NUMERIC NOT NULL, `Q` NUMERIC NOT NULL, FOREIGN KEY(`SIM_ID`) REFERENCES `sim`(`ID`) )")
+    qry.append("CREATE TABLE \"ant_settings\" ( `sim_id` INTEGER NOT NULL UNIQUE, `l` NUMERIC, `sens_offset` NUMERIC, `gain` NUMERIC, `noise_gain` NUMERIC, `sens_fun` TEXT, `deposit_fun` TEXT, FOREIGN KEY(`sim_id`) REFERENCES `sim`(`ID`) ON DELETE CASCADE, PRIMARY KEY(`sim_id`) )")
+    qry.append("CREATE TABLE `deposit_settings` ( `sim_id` INTEGER NOT NULL UNIQUE, `q` numeric, `return_factor` numeric, `beta` numeric, FOREIGN KEY(`sim_id`) REFERENCES `sim`(`ID`) ON DELETE CASCADE, PRIMARY KEY(`sim_id`) )")
+    qry.append("CREATE TABLE `domain_settings` ( `sim_id` INTEGER NOT NULL UNIQUE, `size` TEXT, `pitch` NUMERIC, `nest_loc` TEXT, `nest_rad` integer, `food_loc` TEXT, `food_rad` integer, target_pheromone NUMERIC, FOREIGN KEY(`sim_id`) REFERENCES `sim`(`ID`) ON DELETE CASCADE, PRIMARY KEY(`sim_id`) )")
+    qry.append("CREATE TABLE `gauss_settings` ( `sim_id` INTEGER NOT NULL UNIQUE, `significancy` NUMERIC, `covariance` NUMERIC, FOREIGN KEY(`sim_id`) REFERENCES `sim`(`ID`) ON DELETE CASCADE, PRIMARY KEY(`sim_id`) )")
+    qry.append("CREATE TABLE `queen_settings` ( `sim_id` INTEGER NOT NULL UNIQUE, `default_speed` NUMERIC, `noise_type` TEXT, `noise_parameter` NUMERIC, FOREIGN KEY(`sim_id`) REFERENCES `sim`(`ID`) ON DELETE CASCADE, PRIMARY KEY(`sim_id`) )")
+    qry.append("CREATE TABLE \"results\" ( `sim_id` INTEGER NOT NULL UNIQUE, `entropy_vec` text, `start_entropy` numeric, `end_entropy` numeric, `foodcount` integer, `nestcount` integer, `scorecard` TEXT, `step_vec` TEXT, FOREIGN KEY(`sim_id`) REFERENCES `sim`(`ID`) ON DELETE CASCADE, PRIMARY KEY(`sim_id`) )")
+    qry.append("CREATE TABLE `sens_settings` ( `sim_id` INTEGER NOT NULL UNIQUE, `breakpoint` numeric, `exp_lambda` numeric, FOREIGN KEY(`sim_id`) REFERENCES `sim`(`ID`) ON DELETE CASCADE, PRIMARY KEY(`sim_id`) )")
+    path = '/'.join(sys.argv[0].split('/')[:-1])+'/'
+    create_connection(path+"cythonic/database/"+"stigmergy.db", qry)
